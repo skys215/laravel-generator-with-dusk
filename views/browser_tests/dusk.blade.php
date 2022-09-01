fe6e028132a0656f38c8bd531db929c8cc88c154
skys215 <skys215@gmail.com>
2022-09-01 21:02:28 +0800

    Fixed browser test lang typo

--- views/browser_tests/dusk.blade.php
+++ views/browser_tests/dusk.blade.php
@@ -15,7 +15,7 @@ class {{$config->modelNames->name}}Test extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->loginAs(1)
                     ->visit(route('@if($config->prefixes->route) {{$config->prefixes->getRoutePrefixWith('.')}} @endif{{$config->modelNames->snakePlural}}.index'))
-                    ->assertSee(@if($config->options->localized)__('models/{{ $config->modelNames->camelPlural }}.labels.backend.management') @else '{{$config->modelNames->humanPlural}}'@endif)
+                    ->assertSee(@if($config->options->localized)__('models/{{ $config->modelNames->camelPlural }}.plural') @else '{{$config->modelNames->humanPlural}}'@endif)
                     ->waitFor('{{$table_selector}}');
             $this->capture('index');
         });
@@ -26,7 +26,7 @@ class {{$config->modelNames->name}}Test extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->loginAs(1)
                     ->visit(route('@if($config->prefixes->route) {{$config->prefixes->getRoutePrefixWith('.')}} @endif{{$config->modelNames->snakePlural}}.create'))
-                    ->assertSee(@if($config->options->localized)__('models/{{ $config->modelNames->camelPlural }}.labels.backend.create') @else 'Create {{$config->modelNames->humanPlural}}' @endif);
+                    ->assertSee(@if($config->options->localized)__('crud.create').' '.__('models/{{ $config->modelNames->camelPlural }}.singular') @else 'Create {{$config->modelNames->humanPlural}}' @endif);
             $this->capture('create');
         });
     }
@@ -36,7 +36,7 @@ class {{$config->modelNames->name}}Test extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->loginAs(1)
                     ->visit(route('@if($config->prefixes->route) {{$config->prefixes->getRoutePrefixWith('.')}} @endif{{$config->modelNames->snakePlural}}.edit',1))
-                    ->assertSee(@if($config->options->localized)__('models/{{ $config->modelNames->camelPlural }}.labels.backend.edit') @else 'Edit {{$config->modelNames->human}}' @endif);
+                    ->assertSee(@if($config->options->localized)__('crud.edit').' '.__('models/{{ $config->modelNames->camelPlural }}.singular') @else 'Edit {{$config->modelNames->human}}' @endif);
             $this->capture('edit');
         });
     }
@@ -46,7 +46,7 @@ class {{$config->modelNames->name}}Test extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->loginAs(1)
                     ->visit(route('@if($config->prefixes->route) {{$config->prefixes->getRoutePrefixWith('.')}} @endif{{$config->modelNames->snakePlural}}.show',1))
-                    ->assertSee(@if($config->options->localized)__('models/{{ $config->modelNames->camelPlural }}.labels.backend.show') @else '{{$config->modelNames->human}}  Details'@endif);
+                    ->assertSee(@if($config->options->localized)__('models/{{ $config->modelNames->camelPlural }}.singular').' '.__('crud.detail') @else '{{$config->modelNames->human}}  Details'@endif);
             $this->capture('view');
         });
     }
